f1830743ed94b5e9549eac91ffe40ed1b45e24b1
moneka <monekawilson16@gmail.com>
2025-02-15 00:22:57 +0200

    Most Recently>

--- lib/home/tabs/quran/quran_tab.dart
+++ lib/home/tabs/quran/quran_tab.dart
@@ -3,53 +3,55 @@ import 'package:islami/app-colors.dart';
 import 'package:islami/home/tabs/quran/sura_details_screen.dart';
 import 'package:islami/home/tabs/quran/sura_list_widget.dart';
 import 'package:islami/models/sura_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class QuranTab extends StatefulWidget {
-  QuranTab({super.key});
+  const QuranTab({super.key});
 
   @override
   State<QuranTab> createState() => _QuranTabState();
 }
 
 class _QuranTabState extends State<QuranTab> {
-  void addSuraList() {
-    for (int i = 0; i < 114; i++) {
-      SuraModel.suraList.add(SuraModel(
-          suraEnglishName: SuraModel.suraEnglishList[i],
-          suraArabicName: SuraModel.suraArabicList[i],
-          numOfVerses: SuraModel.numOfVersesList[i],
-          fileName: "${i + 1}.txt"));
-    }
-  }
+  List<SuraModel> filterList = [];
+  String searchText = "";
+  Map<String, String> loadSuraList = {};
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     addSuraList();
+    filterList = List.from(SuraModel.suraList);
+    loadLastSura();
   }
 
-  List<SuraModel> filterList = SuraModel.suraList;
-  String searchText = "";
+  void addSuraList() {
+    if (SuraModel.suraList.isEmpty) {
+      for (int i = 0; i < 114; i++) {
+        SuraModel.suraList.add(SuraModel(
+          suraEnglishName: SuraModel.suraEnglishList[i],
+          suraArabicName: SuraModel.suraArabicList[i],
+          numOfVerses: SuraModel.numOfVersesList[i],
+          fileName: "${i + 1}.txt",
+        ));
+      }
+    }
+    filterList = List.from(SuraModel.suraList);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          margin: const EdgeInsets.all(20),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.asset(
-                "assets/images/quran_logo.png",
-                fit: BoxFit.cover,
-              ),
+              Image.asset("assets/images/quran_logo.png", fit: BoxFit.cover),
               TextField(
                 cursorColor: AppColor.white,
-                style: const TextStyle(
-                  color: AppColor.white,
-                ),
+                style: const TextStyle(color: AppColor.white),
                 decoration: InputDecoration(
                   hintText: "Sura Name",
                   hintStyle: Theme.of(context).textTheme.titleLarge,
@@ -60,102 +62,67 @@ class _QuranTabState extends State<QuranTab> {
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
                     borderSide: const BorderSide(
-                      color: AppColor.primaryColor,
-                      width: 2,
-                    ),
+                        color: AppColor.primaryColor, width: 2),
                   ),
                   focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(
-                        color: AppColor.primaryColor,
-                        width: 2,
-                      )),
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                        color: AppColor.primaryColor, width: 2),
+                  ),
                 ),
                 onChanged: (text) {
-                  searchText = text;
-                  filterList = SuraModel.suraList.where((suraModel) {
-                    return suraModel.suraArabicName.contains(searchText) ||
-                        suraModel.suraEnglishName
-                            .toLowerCase()
-                            .contains(searchText.toLowerCase());
-                  }).toList();
-                  setState(() {});
-                  print(filterList);
+                  setState(() {
+                    searchText = text.toLowerCase();
+                    filterList = SuraModel.suraList.where((sura) {
+                      return sura.suraArabicName.contains(searchText) ||
+                          sura.suraEnglishName
+                              .toLowerCase()
+                              .contains(searchText);
+                    }).toList();
+                  });
                 },
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: Text("Most Recently",
-                    style: Theme.of(context).textTheme.titleLarge),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 10),
-                height: 150,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: AppColor.primaryColor,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Sura En",
-                          style: Theme.of(context).textTheme.bodyLarge,
-                        ),
-                        Text(
-                          "Sura Ar",
-                          style: Theme.of(context).textTheme.bodyLarge,
-                        ),
-                        Text(
-                          "Sura Num",
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                      ],
-                    ),
-                    Image.asset(
-                      "assets/images/sora_most_recent.png",
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Sura List",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
+              const SizedBox(height: 10),
+              searchText.isNotEmpty || loadSuraList["suraEngName"] == ""
+                  ? const SizedBox()
+                  : buildMostRecentlyWidget(context),
+              const SizedBox(height: 10),
+              Text("Sura List", style: Theme.of(context).textTheme.titleLarge),
+              const SizedBox(height: 10),
               Expanded(
                 child: ListView.separated(
-                  separatorBuilder: (context, index) {
-                    return const Divider(
-                      color: AppColor.white,
-                      thickness: 2,
-                      indent: 30,
-                      endIndent: 20,
-                    );
-                  },
+                  key: ValueKey(searchText),
+                  separatorBuilder: (_, __) => const Divider(
+                    color: AppColor.white,
+                    thickness: 2,
+                    indent: 30,
+                    endIndent: 20,
+                  ),
+                  itemCount: filterList.length,
                   itemBuilder: (context, index) {
+                    final sura = filterList[index];
                     return InkWell(
                       onTap: () {
+                        final selectedSura =
+                            filterList[index]; // خليك على الفلتر
+
+                        saveLastSura(
+                          suraEngName: selectedSura.suraEnglishName,
+                          suraArbName: selectedSura.suraArabicName,
+                          numOfVerses: selectedSura.numOfVerses,
+                        );
+
                         Navigator.of(context).pushNamed(
-                            SuraDetailsScreen.routeName,
-                            arguments: filterList[index]);
+                          SuraDetailsScreen.routeName,
+                          arguments: selectedSura,
+                        );
                       },
                       child: SuraListWidget(
-                        suraModel: filterList[index],
+                        suraModel: sura,
                         index: index,
                       ),
                     );
                   },
-                  itemCount: filterList.length,
                 ),
               ),
             ],
@@ -163,5 +130,62 @@ class _QuranTabState extends State<QuranTab> {
         ),
       ),
     );
+  }
+
+  Widget buildMostRecentlyWidget(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Most Recently", style: Theme.of(context).textTheme.titleLarge),
+        Container(
+          margin: const EdgeInsets.only(top: 10),
+          height: 130,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: AppColor.primaryColor,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(loadSuraList["suraEngName"] ?? "",
+                      style: Theme.of(context).textTheme.bodyLarge),
+                  Text(loadSuraList["suraArbName"] ?? "",
+                      style: Theme.of(context).textTheme.bodyLarge),
+                  Text(loadSuraList["numOfVerses"] ?? "",
+                      style: Theme.of(context).textTheme.bodyMedium),
+                ],
+              ),
+              Image.asset("assets/images/sora_most_recent.png",
+                  fit: BoxFit.cover),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> saveLastSura(
+      {required String suraEngName,
+      required String suraArbName,
+      required String numOfVerses}) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("suraEngName", suraEngName);
+    await prefs.setString("suraArbName", suraArbName);
+    await prefs.setString("numOfVerses", numOfVerses);
+    await loadLastSura();
+  }
+
+  Future<void> loadLastSura() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      loadSuraList = {
+        "suraEngName": prefs.getString("suraEngName") ?? "",
+        "suraArbName": prefs.getString("suraArbName") ?? "",
+        "numOfVerses": prefs.getString("numOfVerses") ?? "",
+      };
+    });
   }
 }
